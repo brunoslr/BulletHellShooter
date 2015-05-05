9d2d2758e7c88175cad8caeabb2d41003643b8e6
unknown <bsr1615@OH1385-3.main.ad.rit.edu>
2015-05-05 10:10:34 -0400

    CleanUp - Jam9

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	//public AudioClip start;
+    //public AudioSource source;
+
+	public void StartGame()
+	{
+     //   source.Play();	
+        StartCoroutine("LoadMainLevel");
+	}
+
+	public void Level1()
+	{
+		//source.Play();	
+		StartCoroutine("LoadLevel1");
+	}
+
+	public void Level2()
+	{
+		//source.Play();	
+		StartCoroutine("LoadLevel2");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+    private IEnumerator LoadMainLevel() 
+    {
+        yield return new WaitForSeconds(0.5f);
+        Application.LoadLevel ("level1");
+    }
+
+	private IEnumerator LoadLevel1() 
+	{
+		yield return new WaitForSeconds(0.5f);
+		Application.LoadLevel ("level1");
+	}
+
+	private IEnumerator LoadLevel2() 
+	{
+		yield return new WaitForSeconds(0.5f);
+		Application.LoadLevel ("level2");
+	}
+
+}
